03284033686ad4043381c63db26c1523592a3a21
Jherrnson <jherrnson@wesleyan.edu>
2024-03-06 15:41:18 -0500

    funnnnn

--- starter/lib/interp.ml
+++ starter/lib/interp.ml
@@ -338,7 +338,7 @@ let rec eval (sigmas : Frame.t) (e : E.t) (pgrm : P.t) : (Value.t * Frame.t) =
     end
   | E.Call (x, pl) -> 
     let FunDef (id, p, sl) = funLookup pgrm x in
-      let newEnv, newFrame = functionEnvironmentMaker (FunDef (id, p, sl)) Env.empty pl sigmas in 
+      let newEnv, newFrame = functionEnvironmentMaker (P.FunDef (id, p, sl)) Env.empty pl sigmas pgrm in 
         (Frame.get_value (statement (Frame.push newEnv sigmas) (S.Block sl) pgrm)), newFrame
 (*! eval let !*)
 
@@ -380,13 +380,15 @@ and funLookup (pgrm : P.t) (id : Ast.Id.t) : P.fundef  =
     | Pgm [] -> raise(UndefinedFunction "Function not defined")
     | Pgm (FunDef (name,l,sl) :: tail) -> if name = id then FunDef (name,l,sl) else funLookup(Pgm tail) (id)
 
-and functionEnvironmentMaker (func : P.fundef) (sigma : Env.t) (paramValues : E.t list) (sigmas : Frame.t) : Env.t * Frame.t =
+and functionEnvironmentMaker (func : P.fundef) (sigma : Env.t) (paramValues : E.t list) (sigmas : Frame.t) (pgrm : P.t) : Env.t * Frame.t =
   match func with 
   |(FunDef (_, [], _)) -> sigma, sigmas
   |(FunDef (id, h :: t, sl)) -> begin
     match paramValues with 
     | [] -> failwith("aaa")
-    | head :: tail -> functionEnvironmentMaker (FunDef (id, t, sl)) (Env.update sigma h (eval head)) tail sigmas
+    | head :: tail -> 
+      let v, newFrame = eval sigmas head pgrm in 
+        functionEnvironmentMaker (FunDef (id, t, sl)) (Env.update sigma h v) tail newFrame pgrm
   end
 
 (*  eval e = v, where _ ├ e ↓ v.
